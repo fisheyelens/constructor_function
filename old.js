cff5be74b7651b973294dc345136dce54d696e67
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-03 15:36:56 +0900

    비교

--- old.js
+++ old.js
@@ -1,6 +1,34 @@
 // * 객체 리턴을 올드 방식으로
 
-function old (a,b){
+// function old (a,b){
+// this.first = a;
+// this.first = b;
+// }
 
-  
+// let test = new old(1,2);
+// console.log(test);
+
+
+
+
+// * 밑에는 강사 코드
+
+function old(a, b) {
+  this.first = a;
+  this.second = b;
 }
+
+let test = new old(1,2);
+console.log(test);
+
+// * 여하튼 넘어가서
+
+class NewOld {
+  constructor(a,b){
+    this.first = a;
+    this.second = b;
+  }
+}
+
+let newStyle = new NewOld(1,2);
+console.log(newStyle);
