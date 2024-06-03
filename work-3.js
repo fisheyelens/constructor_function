71421f50a88abe3c205a57563c7e3ddf5aa81d26
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-03 12:05:16 +0900

    또 바꿈

--- work-3.js
+++ work-3.js
@@ -1,9 +1,27 @@
-function workThree(a,b){
-  return {
-    a:a,
-    b:b
-};
-};
+// function workThree(a,b){
+//   let local = {
 
-let test = workThree("구하림","유으뜸");
+// number : index + 1,
+// name : name,
+
+
+//   };
+//   local[a]=a;
+//   local[b]=b;
+//   return local;
+// };
+
+
+// let test = workThree("구하림","유으뜸");
+// console.log(test);
+
+function workThree(index, name) {
+  let local = {
+    number: index + 1,
+    name: name,
+  };
+  return local;
+}
+
+let test = workThree(0, "구하림");
 console.log(test);
