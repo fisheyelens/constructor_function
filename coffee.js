f0d8108a0549fae9b4f1db53add078c924488afe
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-03 15:41:49 +0900

    생성자 함수 기본형

---
+++ coffee.js
@@ -0,0 +1,11 @@
+class MakeCoffee {
+  constructor(wondoo, water, sugar, milk) {
+    this.wondoo = wondoo;
+    this.water = water;
+    this.sugar = sugar;
+    this.milk = milk; 
+  }
+} 
+
+let test = new MakeCoffee("대전", 300, true, true);
+console.log(test);
