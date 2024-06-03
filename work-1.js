f192c0d0d59737872aef8461d22abc500d3ccd16
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-03 11:25:55 +0900

    함수 만들기 시도했었음..

--- work-1.js
+++ work-1.js
@@ -5,5 +5,28 @@
 function workOne(a,b){
 // * logic : 매개변수 2개를 받은 다음에
 // * logic : return은 객체였음 좋겠다.
+// * 지역 변수 : 함수 안에서만 생명이 있는, 통용되는 임시 변수! 
+// * 지역변수가 없으면 이름 작명 할 때마다 불편할 것이다
 
+// 상황 : 지역변수로 객체를 초기화했다.
+
+// 매개변수 a,b 두 개가
+
+// 객체의 키 이름과 값 리터럴이 되었으면 좋겠다.
+
+// hint : 객체 할당 검사 / 매개 변수 두 개 묶어버리기
+// // * 이것 많이 쓸 패턴
+// let localVariable = {
+
+//   a:b
+
+// };
+
+// let lalala = localVariable(a,b);
+
+// return lalala;
+
+// return localVariable;
+
+// console.log(localVariable(1,2));
 }
